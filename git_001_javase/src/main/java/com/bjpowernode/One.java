7572afa7a38bffeee82d5a7db7c3048e18603b10
Administrator <8028@qq.com>
2022-08-18 09:01:37 +0800

    第一次提交两个项目到本地仓库

---
+++ git_001_javase/src/main/java/com/bjpowernode/One.java
@@ -0,0 +1,10 @@
+package com.bjpowernode;
+
+/**
+ *
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.println("这是javaSE项目的功能.......");
+    }
+}
---
+++ git_002_javaweb/src/main/java/com/bjpowernode/pojo/Student.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.pojo;
+
+/**
+ *
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("javaWeb项目的内容...........");
+    }
+}
