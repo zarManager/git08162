3538d96b93527449919f3e304ce8a6c9ae2b5bf6
Administrator <8028@qq.com>
2022-08-18 10:30:41 +0800

    程序员zarencode被邀请后增加和修改了内容

---
+++ git_001_javase/src/main/java/com/bjpowernode/Three.java
@@ -0,0 +1,10 @@
+package com.bjpowernode;
+
+/**
+ *
+ */
+public class Three {
+    public static void main(String[] args) {
+        System.out.println("这是程序员增加的一个类........");
+    }
+}
--- git_002_javaweb/src/main/java/com/bjpowernode/pojo/Student.java
+++ git_002_javaweb/src/main/java/com/bjpowernode/pojo/Student.java
@@ -7,5 +7,7 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("javaWeb项目的内容...........");
         System.out.println("这是项目经理新修改的Student中的内容..........");
+        System.out.println("这是程序员增加的内容.......");
+
     }
 }
